866a80e2b40a1a82766c215611fb0ad242c501e9
fallenworld <fallenworlder@gmail.com>
2015-09-06 13:00:20 +0000

    Car can move now

--- src/server/car.py
+++ src/server/car.py
@@ -1,21 +1,38 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-out=8
-GPIO.setup(out, GPIO.OUT)
+class Car:
+    in1=16
+    in2=18
+    in3=13
+    in4=15
+    ena=11
+    enb=12
 
-state=1
+    def __init__(self):
+        self.speed=3
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+        GPIO.setup([Car.in1, Car.in2, Car.in3, Car.in4], GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup([Car.ena, Car.enb], GPIO.OUT)
+    
+    def move(self):
+        GPIO.output([Car.in1, Car.in2], (GPIO.HIGH, GPIO.LOW))
+        GPIO.output([Car.in3, Car.in4], (GPIO.HIGH, GPIO.LOW))
+        GPIO.output([Car.ena, Car.enb], GPIO.HIGH)
+        #Car.enaOut=GPIO.PWM(Car.ena, 14000)
+        #Car.enbOut=GPIO.PWM(Car.enb, 14000)
+        #Car.enaOut.start(100)
+        #Car.enbOut.start(100)
+        time.sleep(2)
+        GPIO.output([Car.in1, Car.in2, Car.in3, Car.in4], GPIO.LOW)
+        #Car.enaOut.stop()
+        #Car.enbOut.stop()
 
 try:
-    while True:
-        if state==1:
-            GPIO.output(out, 0)
-            state=0
-        else:
-            GPIO.output(out, 1)
-            state=1
-        time.sleep(0.1)
+    car=Car()
+    car.move()
 except KeyboardInterrupt:
     GPIO.cleanup()
+
+GPIO.cleanup()
